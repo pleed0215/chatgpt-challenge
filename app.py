ac3980f3c48646dddfc993a4f8510359f70a4656
pleed0215 <pleed@naver.com>
2024-12-19 16:09:02 +0900

    Sidebar에 upload 추가.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import logging
-from langchain.document_loaders import TextLoader
+import os
+import sys
+from typing import Any
+
+from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
@@ -10,161 +14,213 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 
 import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
+from utils.logger import logger_factory
 from utils.st_cache import set_to_cache, get_from_cache, init_cache
 from utils.chat_stream import Chat, ChatCallbackHandler
 
 CACHE_DIR = './.cache'
-prompt = ChatPromptTemplate.from_messages([
-            ('system', """
-            You are good at replying users questions based on below context.\n\n
-            Beware you have to answer base on the context, otherwise you just say you don't know.
-            context: {context},
-            ------
-            """),
-            MessagesPlaceholder(variable_name='chat_history'),
-            ('human', '{question}'),
-        ])
+UPLOAD_DIR = './uploads'
 
-def config():
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        level=logging.INFO)
-    st.set_page_config(
-        page_title="Chatgpt challenge Day9-11",
-        page_icon="🤖",
-    )
+class App:
+    retriever = None
+
+    prompt = ChatPromptTemplate.from_messages([
+        ('system', """
+                You are good at replying users questions based on below context.\n\n
+                Beware you have to answer base on the context.
+                If you got question before user asked in chat_history, just reply that.
+                context: {context},
+                ------
+                """),
+        MessagesPlaceholder(variable_name='chat_history'),
+        ('human', '{question}'),
+    ])
+
+    def __init__(self):
+        self.config()
+        self.logger = logger_factory("AppLogger")
+        self.init_cache()
+
+    def config(self):
+        st.set_page_config(
+            page_title="Chatgpt challenge Day9-11",
+            page_icon="🤖",
+            layout="centered",
+        )
 
 
+    def init_cache(self):
+        init_cache('messages', [])
+        init_cache('old_api_key', '')
 
-def initialize_cache():
-    init_cache('messages', [])
-    init_cache('old_api_key', '')
 
-def initialize(api_key: str):
-    try:
-        old_api_key = get_from_cache('old_api_key')
-        is_api_key_changed = old_api_key is not None and old_api_key != api_key
-        if is_api_key_changed:
-            st.session_state.clear()
-            st.cache_data.clear()
-            initialize_cache()
+    def initialize(self, api_key: str):
+        try:
+            self.logger.info("Initializing...")
+            old_api_key = get_from_cache('old_api_key')
+            is_api_key_changed = old_api_key is not None and old_api_key != api_key
+            if is_api_key_changed:
+                st.session_state.clear()
+                st.cache_data.clear()
+                self.init_cache()
 
-        store = get_from_cache('store')
-        if not store or is_api_key_changed:
-            logging.info("Loading context and embeddings...")
-            text_loader = TextLoader('./documents/document.txt')
-            text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-                separators=[
-                    '\n\n', '\n'
-                ],
+            memory = get_from_cache('memory')
+            if not memory:
+                self.logger.info("Making memory for chat history...")
+                memory = ConversationBufferMemory(
+                    memory_key='chat_history',
+                    return_messages=True,
+                )
+                set_to_cache('memory', memory)
+
+            llm = get_from_cache('llm')
+            if not llm or is_api_key_changed:
+                self.logger.info("Creating llm...")
+                llm = ChatOpenAI(
+                    model='gpt-4o-mini',
+                    api_key=api_key,
+                    streaming=True,
+                    callbacks=[
+                        ChatCallbackHandler(),
+                    ]
+                )
+                set_to_cache('llm', llm)
+            self.logger.info("Initialization complete..")
+        except Exception as e:
+            st.toast(e, icon='🚨')
+            self.logger.error('Failed to initialize langchain stuff...')
+
+    def ask_something(self, question: str) -> str|None:
+        try:
+            self.logger.info('Human: %s', question)
+            if self.retriever is None:
+                self.logger.error('Retriever not initialized.')
+            memory = get_from_cache('memory')
+            llm = get_from_cache('llm')
+
+            if not memory or not llm:
+                self.logger.error("Memory or LLM not initialized.")
+                return None
+
+            chain = {"question": RunnablePassthrough(),
+                     "context": self.retriever,
+                     'chat_history': RunnableLambda(
+                         lambda _: memory.load_memory_variables({})[
+                             'chat_history'])} | self.prompt | llm
+
+            answer = chain.invoke(question)
+            self.logger.info('ai answered: {}'.format(answer.content))
+
+            return answer.content
+        except Exception as e:
+            st.toast(e, icon="😵")
+            self.logger.error('Failed to generate answer...{}'.format(e))
+            return None
+
+    def on_click_clear(self):
+        if 'api_key' in st.session_state:
+            set_to_cache('api_key', '')
+            set_to_cache('messages', [])
+            set_to_cache('old_api_key', '')
+            set_to_cache('logs', [])
+        st.cache_data.clear()
+        self.retriever = None
+        self.logger.info("Cleared state and cache.")
+
+    @st.cache_data(show_spinner="Now uploading and embedding your document.")
+    def embed_file(_self, uploaded_file: UploadedFile) -> Any:
+        api_key = get_from_cache('api_key')
+        if api_key is None:
+            return None
+
+        file_content = uploaded_file.read()
+        file_path = os.path.join(UPLOAD_DIR, uploaded_file.name)
+
+        # make sure UPLOAD_DIR and CACHE_DIR exist.
+        if not os.path.exists(UPLOAD_DIR):
+            os.makedirs(UPLOAD_DIR, exist_ok=True)
+        if not os.path.exists(CACHE_DIR):
+            os.makedirs(CACHE_DIR, exist_ok=True)
+
+        with open(file_path, 'wb') as f:
+            f.write(file_content)
+
+            embedding_dir = os.path.join(CACHE_DIR, uploaded_file.name)
+            local_store = LocalFileStore(embedding_dir)
+            unstructured_loader = UnstructuredFileLoader(file_path)
+            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                separators=['\n\n', '\n'],
                 chunk_size=600,
                 chunk_overlap=100,
             )
-            docs = text_loader.load_and_split(text_splitter=text_splitter)
+            split_docs = unstructured_loader.load_and_split(
+                text_splitter=splitter)
 
-            embeddings = OpenAIEmbeddings(
-                openai_api_key=api_key,
-            )
-            cache_store = LocalFileStore(CACHE_DIR)
-            cache_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings,
-                                                                      cache_store)
-            vector_store = FAISS.from_documents(docs, cache_embeddings)
+            embeddings = OpenAIEmbeddings(api_key=api_key)
+            cache_backed = CacheBackedEmbeddings.from_bytes_store(embeddings,
+                                                                  local_store)
+            vector_store = FAISS.from_documents(split_docs, cache_backed)
+            return vector_store.as_retriever()
 
-            set_to_cache('store', vector_store)
+    def run(self):
+        st.title("Chatgpt challenge day9-11")
 
-        memory = get_from_cache('memory')
-        if not memory:
-            logging.info("Making memory for chat history...")
-            memory = ConversationBufferMemory(
-                memory_key='chat_history',
-                return_messages=True,
-            )
-            set_to_cache('memory', memory)
+        with st.sidebar:
+            text = st.text_input("Write your own OPEN_API_KEY. We won't save it",
+                          key='api_key')
+            st.button("Submit", use_container_width=True, )
 
 
-        llm = get_from_cache('llm')
-        if not llm or is_api_key_changed:
-            logging.info("Creating llm...")
-            llm = ChatOpenAI(
-                model='gpt-4o-mini',
-                api_key=api_key,
-                streaming=True,
-                callbacks=[
-                    ChatCallbackHandler(),
-                ]
-            )
-            set_to_cache('llm', llm)
+            if text:
+                st.success("API key has been ready!")
+                st.button("Clear API Key", use_container_width=True, on_click=self.on_click_clear)
 
-    except Exception as e:
-        st.toast(e, icon='🚨')
-        logging.error('Failed to initialize langchain stuff...')
+                file = st.file_uploader("Upload your document text file!")
+                if file:
+                    self.retriever = self.embed_file(file)
+                    self.logger.info("Uploaded file embedding done!")
 
-
-def ask_something(question: str) -> str | None:
-    try:
-        logging.info('User asking question...{}'.format(question))
-        store = get_from_cache('store')
-        memory = get_from_cache('memory')
-        llm = get_from_cache('llm')
-
-        chain = {"question": RunnablePassthrough(),
-                 "context": store.as_retriever(),
-                 'chat_history': RunnableLambda(
-                     lambda _: memory.load_memory_variables({})[
-                         'chat_history'])} | prompt | llm
-
-        answer = chain.invoke(question)
-        logging.info('ai answered: {}'.format(answer.content))
-
-        return answer.content
-    except Exception as e:
-        st.toast(e, icon="😵")
-        logging.error('Failed to generate answer...{}'.format(e))
-        return None
-
-def on_click_clear():
-    if 'api_key' in st.session_state:
-        set_to_cache('api_key', '')
-        set_to_cache('messages', [])
-    st.cache_data.clear()
-
-def main():
-    st.title("Chatgpt challenge day9-11")
-
-    with st.sidebar:
-        st.text_input("Write your own OPEN_API_KEY. We won't save it", key='api_key')
-        col1, col2 = st.columns(2)
-        submit_button = col1.button("Submit", use_container_width=True,)
-        col2.button("Clear", use_container_width=True,
-                                   on_click=on_click_clear)
+                    # st.markdown("### Logs...")
+                    # container = st.container(height=300, border=True)
+                    # with container:
+                    #     logs = get_from_cache('logs')
+                    #     if not logs:
+                    #         st.text("No logs yet...")
+                    #     else:
+                    #         for log in logs:
+                    #             st.text(log)
 
 
-    api_key = get_from_cache('api_key')
-    if api_key:
-        initialize(api_key=api_key)
-        set_to_cache('old_api_key', api_key)
-        st.success('Initialization complete.')
+        if self.retriever:
+            api_key = get_from_cache('api_key')
+            self.initialize(api_key=api_key)
+            set_to_cache('old_api_key', api_key)
+            st.success('Initialization complete.')
 
-        Chat.send_message("I'm ready to answer your question.",
-                          role='ai')
-        Chat.paint_messages()
-        chat_message = st.chat_input(
-            "Ask anything about document uploaded.")
-        if chat_message:
-            Chat.send_message(chat_message, role='human', save=True)
+            chat_message = st.chat_input(
+                "Ask anything about document uploaded.")
+            Chat.send_message("I'm ready to answer your question.",
+                              role='ai')
+            Chat.paint_messages()
 
-            with st.chat_message('ai'):
-                answer = ask_something(chat_message)
-                if answer:
-                    memory = get_from_cache('memory')
-                    memory.save_context(
-                        inputs={'human': chat_message},
-                        outputs={'ai': answer},
-                    )
-                    logging.info("History: {}".format(memory.load_memory_variables({})['chat_history']))
+            if chat_message:
+                Chat.send_message(chat_message, role='human',
+                                  save=True)
 
-# Main loop
-config()
-initialize_cache()
-main()
+                with st.chat_message('ai'):
+                    answer = self.ask_something(chat_message)
+                    if answer:
+                        memory = get_from_cache('memory')
+                        memory.save_context(
+                            inputs={'human': chat_message},
+                            outputs={'ai': answer},
+                        )
+
+
+
+
+
+app = App()
+app.run()
